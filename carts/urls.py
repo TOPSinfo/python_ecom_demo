2207ebf1b5139f66c59131cc72a187b690061c2a
ajayjain <ajayjain@topsinfosolutions.com>
2022-11-01 18:19:10 +0530

    Added new files

---
+++ carts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('',views.cart,name="carts"),
+    path('add_cart/<int:product_id>/',views.add_cart,name="add_cart"),
+    path('decrement_cart/<int:product_id>/<int:cart_item_id>/',views.decrement_cart,name="decrement_cart"),
+    path('remove_cart/<int:product_id>/<int:cart_item_id>/',views.remove_cart,name="remove_cart"),
+
+]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,13 @@
+from itertools import product
 from unicodedata import category
 from django.shortcuts import render , get_object_or_404
+from carts.models import Cart, CartItem
 from category.models import Category
 from .models import Product
-
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 
@@ -13,14 +18,16 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.all().filter(category= categories,is_available=True)
-        product_count = products.count()
-
     else:
-        products = Product.objects.all().filter(is_available=True)
-        product_count = products.count()
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        
+    paginator = Paginator(products,4)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count
     }
 
@@ -31,10 +38,34 @@ def product_details(request, category_slug,product_slug):
 
     try:
         product_details = Product.objects.get(category__slug= category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request),product= product_details).exists()
+
     except Exception as e:
         raise e
     
     context = {
-        "product_details" : product_details
+        "product_details" : product_details,
+        "in_cart" : in_cart
     }
     return render(request, 'store/product_details.html',context)
+
+
+
+
+def search(request):
+
+    products = Product.objects.none()
+
+    if 'keyword' in request.GET:
+        keyword =  request.GET['keyword']
+        if keyword:
+            print(keyword)
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+    
+    print(products)
+    context = {
+        'products': products,
+        'product_count': products.count()
+    }
+
+    return render(request, 'store/store.html',context)
